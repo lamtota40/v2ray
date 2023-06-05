843dcfc6cca57b8f3904b05e965c306142e3640f
lamtota40 <lamtota40@gmail.com>
2023-06-05 12:01:52 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -28,7 +28,8 @@ IZIN=$( curl https://raw.githubusercontent.com/godtrex99/ip/main/ipvps | grep $M
 clear
 MYIP=$(wget -qO- ipinfo.io/ip);
 clear
-domain=$(cat /etc/xray/domain)
+#domain=$(cat /etc/xray/domain)
+domain=$(cat /root/myvpn/domain)
 apt install iptables iptables-persistent -y
 apt install curl socat xz-utils wget apt-transport-https gnupg gnupg2 gnupg1 dnsutils lsb-release -y 
 apt install socat cron bash-completion ntpdate -y
