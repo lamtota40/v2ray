306e2cb84d4e2643aaaf686d47bd78fa3a2e6af6
lamtota40 <lamtota40@gmail.com>
2023-06-05 12:17:32 +0700

    Create setup2.sh

---
+++ setup2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+domain=$(cat /root/myvpn/domain)
+apt install iptables iptables-persistent -y
+apt install curl socat xz-utils wget apt-transport-https gnupg gnupg2 gnupg1 dnsutils lsb-release -y 
+apt install socat cron bash-completion ntpdate -y
+ntpdate pool.ntp.org
+apt -y install chrony
+timedatectl set-ntp true
+systemctl enable chronyd && systemctl restart chronyd
+systemctl enable chrony && systemctl restart chrony
+timedatectl set-timezone Asia/Jakarta
+chronyc sourcestats -v
+chronyc tracking -v
+date
